40effd9fd5df9d9017feaf13b1e6adb582a38849
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-02-20 16:04:16 -0700

    Axios Request for photos successfully hit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const dotenv - require('dotenv')
+const dotenv = require('dotenv')
 const cors = require('cors')
-const {json} = require('body-parser')
+const { json } = require('body-parser')
 const axios = require('axios')
 
 const app = express()
@@ -9,9 +9,22 @@ const app = express()
 app.use(cors())
 app.use(json())
 
+const { parsed: config } = dotenv.config()
+
+const BASE_URL = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/resources/image`
+const auth = {
+  username: config.API_KEY,
+  password: config.API_SECRET,
+}
+
 app.get(`/photos`, async (req, res) => {
-    return res.send({message: 'hello'})
+  const response = await axios.get(BASE_URL, {
+    auth,
+  })
+
+  return res.send(response.data)
 })
 
-const PORT = 7000
-app.listen(PORT, conosle.log(`Server running on port ${PORT}`))
+const PORT = 7001
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`))
